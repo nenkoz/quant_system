57387c5c9b524b67878b1a95924b8c1e42c223fc
nenkoz <97909538+nenkoz@users.noreply.github.com>
2023-04-27 12:31:35 -0300

    basic TradeClient functions implemented for communicating with oanda

--- brokerage/oanda/TradeClient.py
+++ brokerage/oanda/TradeClient.py
@@ -1,5 +1,8 @@
 # aids in executing orders in oanda
 
+import json
+import pandas as pd
+import datetime
 import oandapyV20
 import oandapyV20.endpoints.orders as orders
 import oandapyV20.endpoints.trades as trades
@@ -7,6 +10,8 @@ import oandapyV20.endpoints.pricing as pricing
 import oandapyV20.endpoints.accounts as accounts
 import oandapyV20.endpoints.positions as positions
 import oandapyV20.endpoints.instruments as instruments
+
+from collections import defaultdict
 
 
 class TradeClient():
@@ -27,25 +32,99 @@ class TradeClient():
     """
 
     def get_account_details(self):
-        pass
+        try:
+            return self.client.request(accounts.AccountDetails(self.id))["account"]
+        except Exception as err:
+            print(err)
 
     def get_account_summary(self):
-        pass
+        try:
+            return self.client.request(accounts.AccountSummary(self.id))["account"]
+        except Exception as err:
+            print(err)
+
+    def get_account_instruments(self):
+        # the list of tradable instruments for a given account
+        try:
+            r = self.client.request(accounts.AccountInstruments(accountID=self.id))["instruments"]
+            instruments = {}
+            currencies, cfds, metals = [], [], []
+            tags = defaultdict(list)
+            for inst in r:
+                inst_name = inst["name"]
+                type = inst["type"]
+                tag_name = inst["tags"][0]["name"]
+                tags[tag_name].append(inst_name)
+                instruments[inst_name] = {
+                    # we can store other variables for an instrument such as precision and margin rate
+                    "type" : type,
+                    "tag" : inst["tags"][0]["name"]
+                }
+                if type == "CFD":
+                    cfds.append(inst_name)
+                elif type == "CURRENCY":
+                    currencies.append(inst_name)
+                elif type == "METAL":
+                    metals.append(inst_name)
+                else:
+                    print("unknown type: ", inst_name, type)
+                    exit()
+            return instruments, currencies, cfds, metals, tags
+        except Exception as err:
+            print(err)
 
     def get_account_capital(self):
-        pass
+        try:
+            return float(self.get_account_summary()["NAV"])
+        except Exception as err:
+            print(err)
 
     def get_account_positions(self):
-        pass
+        positions_data = self.get_account_details()["positions"]
+        positions = {}
+        for entry in positions_data:
+            instrument = entry["instrument"]
+            long_pos = float(entry["long"]["units"])
+            short_pos = float(entry["short"]["units"])
+            net_pos = long_pos + short_pos
+            if net_pos != 0:
+                positions[instrument] = net_pos
+
+        return positions
 
     def get_account_trades(self):
-        pass
+        try:
+            trade_data = self.client.request(trades.OpenTrades(accountID=self.id))
+            return trade_data
+        except Exception as err:
+            print(err)
 
-    def get_account_orders(self):
-        pass
+    def format_date(self, series):
+        # converting series in the form :: '2022-12-07T22:00:00.000000000Z'
+        ddmmyy = series.split("T")[0].split("-")
+        return datetime.date(int(ddmmyy[0]), int(ddmmyy[1]), int(ddmmyy[2]))
+
 
     def get_ohlcv(self, instrument, count, granularity):
-        pass
+        try:
+            params = {"count": count, "granularity": granularity}
+            candles = instruments.InstrumentsCandles(instrument=instrument, params=params)
+            self.client.request(candles)
+            ohlcv_dict = candles.response["candles"]
+            print(ohlcv_dict)
+            ohlcv = pd.DataFrame(ohlcv_dict)
+            ohlcv = ohlcv[ohlcv["complete"]] # taking only the completed candles
+            ohlcv_df = ohlcv["mid"].dropna().apply(pd.Series) # converts the dictionary in every row into a separate columns
+            ohlcv_df["volume"] = ohlcv["volume"]
+            ohlcv_df.index = ohlcv["time"]
+            ohlcv_df = ohlcv_df.apply(pd.to_numeric) # converts the object data types into numeric
+            ohlcv_df.reset_index(inplace=True) # adds an index 0,1,2,...
+            ohlcv_df.columns = ["date", "open", "high", "low", "close", "volume"]
+            ohlcv_df["date"] = ohlcv_df["date"].apply(lambda x: self.format_date(x))
+        except Exception as err:
+            print(err)
+
+        print(ohlcv_df)
 
     def market_order(self, inst, order_config={}):
         pass
--- main.py
+++ main.py
@@ -19,8 +19,20 @@ print(df, instruments)
 VOL_TARGET = 0.20
 sim_start = df.index[-1] - relativedelta(years=1)
 
-trade_client = Oanda(auth_config=auth_config)
-print(trade_client)
+oanda = Oanda(auth_config=auth_config)
+
+trade_client = oanda.get_trade_client()
+
+trade_client.get_ohlcv("USD_JPY", 100, "D")
+exit()
+instruments, currencies, cfds, metals, tags = trade_client.get_account_instruments()
+
+print(currencies)
+print(cfds)
+print(metals)
+print(tags)
+# results = trade_client.get_account_instruments()
+# print(json.dumps(results, indent= 2))
 exit()
 
 strat = Lbmom(instruments_config="./subsystems/LBMOM/config.json", historical_df=df, simulation_start=sim_start, vol_target=VOL_TARGET)
